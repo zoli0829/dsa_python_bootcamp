9d289ed1cb50bbb4f78c59e7c44c9b8e18993406
Zoltan Vegh <zoli0829@gmail.com>
2024-08-09 18:50:10 +0100

    rearranged files

---
+++ Lists/calcAvgTemp.py
@@ -0,0 +1,19 @@
+# Calculate avg temperature
+
+numDays = int(input("How many days temperature? "))
+total = 0
+temp = []
+for i in range(numDays):
+    nextDay = int(input("Day " + str(i+1) + "'s high temp:"))
+    temp.append(nextDay)
+    total += temp[i]
+
+avg = round(total/numDays,2)
+print("\nAverage = " +str(avg))
+
+above = 0
+for i in temp:
+    if i > avg:
+        above += 1
+
+print(str(above) + " day(s) above average")
